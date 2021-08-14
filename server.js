296f28384dc31f1e19e8cdc522b127ca3cfb2e87
Dana Mulyana <danabontot@gmail.com>
2021-08-14 20:35:13 +0700

    initial setup

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('connected to monggo db'))
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000)
